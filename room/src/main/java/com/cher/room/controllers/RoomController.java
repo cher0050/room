8c76183b41134054753e15fa3cfd2ecb072e51ca
ganak <ganak@localhost>
2024-10-18 06:26:12 +0530

    edit1

---
+++ room/src/main/java/com/cher/room/controllers/RoomController.java
@@ -0,0 +1,13 @@
+package com.cher.room.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/room")
+public class RoomController {
+
+	@GetMapping("/hello")
+	public String displayHello() {
+		return "Hello guys";
+	}
+}
